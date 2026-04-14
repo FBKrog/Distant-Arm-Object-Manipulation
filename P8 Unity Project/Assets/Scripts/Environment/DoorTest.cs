2eef9e3ca2b485b8ed64e0bb1c3dc7969963c9a5
Jakob Winkel Missel <113978150+JakobMissel@users.noreply.github.com>
2026-04-14 14:00:27 +0200

    Environment Building + Added sound to Door Animation

---
+++ P8 Unity Project/Assets/Scripts/Environment/DoorTest.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class DoorTest : MonoBehaviour
+{
+    [SerializeField] AudioClip audioClip;
+    
+    public void DoorSound()
+    {
+        AudioManager.PlaySound(audioClip,transform, 1);
+    }
+}
