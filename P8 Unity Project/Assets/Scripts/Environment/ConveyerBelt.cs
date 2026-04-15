00bb305f185d73171f850cbd35b4c94ebcfbb0fc
Akaidus <nikolajjensen6854@gmail.com>
2026-04-15 15:10:50 +0200

    Conveyer belt

---
+++ P8 Unity Project/Assets/Scripts/Environment/ConveyerBelt.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class ConveyerBelt : MonoBehaviour
+{
+    [SerializeField] bool isActive = true;
+    [SerializeField] GameObject[] belt;
+    [SerializeField] float speed = 1f;
+
+    void Update()
+    {
+        if(!isActive) return;
+        foreach(var cyllinder in belt)
+        {
+            cyllinder.transform.localRotation *= Quaternion.Euler(0, speed * Time.deltaTime * -360, 0);
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if(isActive && other != null && !other.CompareTag("Immovable"))
+        {
+            var rb = other.attachedRigidbody;
+            var moveDir = transform.forward * speed;
+            var clampedMoveDir = Vector3.ClampMagnitude(rb.linearVelocity + moveDir, speed);
+            rb.linearVelocity = new Vector3(clampedMoveDir.x, rb.linearVelocity.y, clampedMoveDir.z);
+        }
+    }
+}
