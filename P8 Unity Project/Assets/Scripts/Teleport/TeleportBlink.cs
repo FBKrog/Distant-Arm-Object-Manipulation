5de4f4730ebe4b19eec6b0cf6838005193fff46a
Akaidus <nikolajjensen6854@gmail.com>
2026-04-28 18:32:55 +0200

    Blink hotfix

--- P8 Unity Project/Assets/Scripts/Teleport/TeleportBlink.cs
+++ P8 Unity Project/Assets/Scripts/Teleport/TeleportBlink.cs
@@ -82,8 +82,8 @@ public class TeleportBlink : MonoBehaviour
         if (_activator != null)
             _activator.onBeforeTeleport = null;
 
-        if (_fadeMaterial != null)
-            Destroy(_fadeMaterial);
+        //if (_fadeMaterial != null)
+        //    Destroy(_fadeMaterial);
     }
 
     // -------------------------------------------------------------------------
