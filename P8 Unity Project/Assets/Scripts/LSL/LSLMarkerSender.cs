e0ea6d0ee98a207590bcf7de3e04e879079864eb
Akaidus <nikolajjensen6854@gmail.com>
2026-05-06 13:58:10 +0200

    LSL purge

--- P8 Unity Project/Assets/Scripts/LSL/LSLMarkerSender.cs
+++ P8 Unity Project/Assets/Scripts/LSL/LSLMarkerSender.cs
@@ -3,17 +3,16 @@ using LSL;
 
 public class LSLMarkerSender : MonoBehaviour
 {
-    private StreamOutlet bvpOutlet;
     private StreamOutlet markerOutlet;
 
     [Header("BVP Settings")]
-    public float samplingRate = 100f; // Hz
-    private float nextSampleTime = 0f;
+    [SerializeField] string streamName = "OpenSignals";
+    [SerializeField] float samplingRate = 1000f; // Hz
 
     void Start()
     {
         var markerInfo = new StreamInfo(
-            "Markers",              // stream name
+            streamName,              // stream name
             "Markers",              // type
             1,                      // one channel
             0,                      // irregular sampling (events)
@@ -22,26 +21,17 @@ public class LSLMarkerSender : MonoBehaviour
         );
 
         markerOutlet = new StreamOutlet(markerInfo);
-        Debug.Log("LSL Marker stream initialized.");
-    }
-
-    void Update()
-    {
-        // --- TEST INPUTS ---
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            SendMarker("Task1_Start");
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            SendMarker("Task1_End");
-        }
+        SendMarker("Session Started");
     }
 
     public void SendMarker(string label)
     {
         markerOutlet.push_sample(new string[] { label });
         Debug.Log("Marker sent: " + label);
+    }
+
+    void OnApplicationQuit()
+    {
+        SendMarker("Session Ended");
     }
 }
